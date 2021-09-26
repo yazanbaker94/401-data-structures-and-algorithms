d40e5dee3f506361071e4882f9e2d1a0b8b5795d
Yazan Baker <yazanbaker94@hotmail.com>
2021-09-26 20:50:18 +0300

    done

--- graph/README.md
+++ graph/README.md
@@ -1,28 +1,15 @@
-## RESTy
-Implement your own Graph. The graph should be represented as an adjacency list, and should include the following methods:
+## GRAPH BREADTH SEARCH
+Feature Tasks
+Write the following method for the Graph class:
 
-add node
-Arguments: value
-Returns: The added node
-Add a node to the graph
-add edge
-Arguments: 2 nodes to be connected by the edge, weight (optional)
-Returns: nothing
-Adds a new edge between two nodes in the graph
-If specified, assign a weight to the edge
-Both nodes should already be in the Graph
-get nodes
-Arguments: none
-Returns all of the nodes in the graph as a collection (set, list, or similar)
-get neighbors
-Arguments: node
-Returns a collection of edges connected to the given node
-Include the weight of the connection in the returned collection
-size
-Arguments: none
-Returns the total number of nodes in the graph
+breadth first
+Arguments: Node
+Return: A collection of nodes in the order they were visited.
+Display the collection
+
+
 
 
 TEST:
 
-![graphtests](graphtests.PNG)
+![GRAPH-breadth](GRAPH-breadth.PNG)
--- graph/graph.js
+++ graph/graph.js
@@ -1,117 +1,72 @@
-const Vertex = require('./vertex');
-const Edge = require('./edge');
+// DATA
+const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
 
-// using Adjacency List way
-class Graph {
-    constructor() {
-        this.adjacencyList = new Map();
-    }
-
-    addVertex(vertex) {
-        this.adjacencyList.set(vertex, []);
-    }
-
-    addNode(node) {
-        this.adjacencyList.set(node, []);
-      }
-
-      addEdge(startVertex, endVertex) {
-        let weight = 0;
-        if (!this.adjacencyList.has(startVertex)) {
-          throw new Error('the graph has no vertex');
-        }
-        const adjacencies = this.adjacencyList.get(startVertex);
-        adjacencies.push(new Edge(endVertex, weight));
-      }
-
-      getAllNodes() {
-        let result = [];
-            for (const [node, edge] of this._adjacencyList.entries()) {
-             result.push(node);
-            }
-            return result;
-      }
+const routes = [
+    ['PHX', 'LAX'],
+    ['PHX', 'JFK'],
+    ['JFK', 'OKC'],
+    ['JFK', 'HEL'],
+    ['JFK', 'LOS'],
+    ['MEX', 'LAX'],
+    ['MEX', 'BKK'],
+    ['MEX', 'LIM'],
+    ['MEX', 'EZE'],
+    ['LIM', 'BKK'],
+];
 
 
-    addDirectedEdge(start, end, weight) {
-        if (!this.adjacencyList.has(start) || !this.adjacencyList.has(end)) {
-            console.log("VERTEX DOES NOT EXIST");
-            return;
-        }
-        // they exist
-        const adjacencies = this.getNeighbours(start) // get me the neighbours of start
-        adjacencies.push(new Edge(end, weight));
-    }
+// The graph
+const adjacencyList = new Map();
 
-    getNeighbours(vertex) {
-        if (!this.adjacencyList.has(vertex)) {
-            console.log("vertex does not exist")
-            return;
-        }
-        return this.adjacencyList.get(vertex);
-    }
-
-    size(node) {
-        const queue = [];
-        const nodes = new Set();
-    
-        queue.unshift(node);
-        nodes.add(node);
-        
-        while (queue.length) {
-          const currentNode = queue.pop();
-          const neighbors = this.getNeighbours(currentNode);
-    
-          for (let neighbor of neighbors) {
-            const neighborNodes = neighbor.vertex;
-    
-            if (nodes.has(neighborNodes)) {
-              continue;
-            } else {
-              nodes.add(neighborNodes);
-              queue.unshift(neighborNodes);
-            }
-          }
-        }
-        return nodes.size;
-      }
-    }
-    
-
-const myGraph = new Graph();
-const zero = new Vertex(0);
-const one = new Vertex(1);
-const two = new Vertex(2);
-const three = new Vertex(3);
-const four = new Vertex(4);
-const five = new Vertex(5);
-
-myGraph.addVertex(zero);
-myGraph.addVertex(one);
-myGraph.addVertex(two);
-myGraph.addVertex(three);
-myGraph.addVertex(four);
-myGraph.addVertex(five);
-
-myGraph.addDirectedEdge(zero, two);
-myGraph.addDirectedEdge(two, four);
-myGraph.addDirectedEdge(two, three);
-myGraph.addDirectedEdge(four, five);
-myGraph.addDirectedEdge(one, three);
-myGraph.addDirectedEdge(five, three);
-
-console.log("------ neighbours of 2------")
-console.log(myGraph.getNeighbours(two));
-
-for (const [k,v] of myGraph.adjacencyList.entries()) {
-    console.log("k = ", k, " v =", v);
+// Add node
+function addNode(airport) {
+    adjacencyList.set(airport, []);
 }
 
+// Add edge, undirected
+function addEdge(origin, destination) {
+    adjacencyList.get(origin).push(destination);
+    adjacencyList.get(destination).push(origin);
+}
+
+// Create the Graph
+airports.forEach(addNode);
+routes.forEach(route => addEdge(...route))
 
 
 
-module.exports = {
-    Vertex,
-    Edge,
-    Graph
-  };
+function bfs(start) {
+
+  const visited = new Set();
+
+  const queue = [start]
+
+
+  while (queue.length > 0) {
+
+      const airport = queue.shift(); // mutates the queue
+
+      const destinations = adjacencyList.get(airport);
+
+
+      for (const destination of destinations) {
+;
+
+          if (destination === 'BKK')  {
+              console.log(`BFS found Bangkok!`)
+          }
+
+          if (!visited.has(destination)) {
+              visited.add(destination);
+              queue.push(destination);
+          }
+         
+      }
+
+      
+  }
+
+}
+
+bfs('PHX')
+
